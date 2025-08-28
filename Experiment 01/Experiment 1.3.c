d5374b46d8c5788818bb7c7118ac99edad729366
Anshita Vardhan <vardhan.anshita104@gmail.com>
2025-08-28 18:48:24 +0530

    Create Experiment 1.3.c

---
+++ Experiment 01/Experiment 1.3.c
@@ -0,0 +1,10 @@
+#include<stdio.h> 
+int main () 
+{ 
+int a=10, b,sum; 
+printf("enter the number b"); 
+scanf("%d", &b); 
+sum= a+b; 
+printf("sum of the two numbers is: %d\n",sum); 
+return 0; 
+}
