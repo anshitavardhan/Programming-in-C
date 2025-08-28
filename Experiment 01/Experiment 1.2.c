dd8736dda586117e848f4243f33c1debdbfa28ac
Anshita Vardhan <vardhan.anshita104@gmail.com>
2025-08-28 18:47:40 +0530

    Create Experiment 1.2.c

---
+++ Experiment 01/Experiment 1.2.c
@@ -0,0 +1,9 @@
+#include<stdio.h> 
+int main () 
+{ 
+printf("Name- Anshita Vardhan\n"); 
+printf("123, Crystal Grand\n"); 
+printf("Dehradun, Uttarakhand\n"); 
+printf("India-248001\n"); 
+return 0; 
+}
