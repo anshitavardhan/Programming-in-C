25f1f279c0a047c53acf8310a7357a6054acb590
Anshita Vardhan <vardhan.anshita104@gmail.com>
2025-09-25 20:02:39 +0530

    Add files via upload

---
+++ Experiment 01/Experiment 1.2.c
@@ -0,0 +1,14 @@
+/*Experiment 1: Installation, Environment Setup and starting with 
+C language
+2. Write a C Program to print the address in multiple lines (new 
+line).*/
+
+#include<stdio.h>
+int main ()
+{
+printf("Name- Anshita Vardhan\n");
+printf("123, Crystal Grand\n");
+printf("Dehradun, Uttarakhand\n");
+printf("India-248001\n");
+return 0;
+}
