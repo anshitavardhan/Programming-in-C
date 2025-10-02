5ed07bbc1f4133796ee0d3b9ac3246fca3a3ce72
Anshita Vardhan <vardhan.anshita104@gmail.com>
2025-10-02 20:11:01 +0530

    Add files via upload

---
+++ Experiment 05/Experiment 5.2.c
@@ -0,0 +1,16 @@
+/*4.2. WAP to print the multiplication table of the number entered by the user. It should be in the correct formatting. Num * 1 = Num*/
+
+
+#include <stdio.h>
+
+int main() {
+    int num;
+    scanf("%d", &num);
+
+    for (int i = 1; i <= 10; i++) {
+        printf("%d * %d = %d\n", num, i, num * i);
+    }
+
+    return 0;
+}
+
