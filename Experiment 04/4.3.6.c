ddc501120258af529363a3af7018d74a827c0fa2
Anshita Vardhan <vardhan.anshita104@gmail.com>
2025-10-19 14:13:48 +0530

    Add files via upload

---
+++ Experiment 04/4.3.6.c
@@ -0,0 +1,23 @@
+/*4.3.6*/
+
+#include <stdio.h>
+int main() {
+    int i, j, n = 5;
+    for (i = n; i >= 1; i--) {
+        for (j = i; j >= 1; j--) {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
